8e0a35eeba6e301672f3223b341f0a4064cdeac1
Ti Chesley <mattiaschesley@gmail.com>
2013-04-13 17:01:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kohab::Application.config.secret_token = '41368ecb4032edbda9f5ee99f69be3d9bf85c8a8e18a3d11a7fc653f7db201efc9222d060e8199fbb2bb93c3b345a571ec09d2f8f3f137eb89feb1b334bb9348'
